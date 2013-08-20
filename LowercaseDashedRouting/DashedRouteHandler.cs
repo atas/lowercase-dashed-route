184844152010df1059932e10f8bef5f30b80ad0e
Ata <ata@ata.io>
2013-08-20 15:15:39 +0300

    Bug Fix of Issue #1 in GitHub (no dashes when Controller is not specified in Html.ActionLink for current Controller)

--- LowercaseDashedRouting/DashedRouteHandler.cs
+++ LowercaseDashedRouting/DashedRouteHandler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -15,10 +17,43 @@ namespace LowercaseDashedRouting
         {
             var values = requestContext.RouteData.Values;
 
-            values["action"] = values["action"].ToString().Replace("-", "");
-            values["controller"] = values["controller"].ToString().Replace("-", "");
+            values["action"] = UnDash(values["action"].ToString());
+            values["controller"] = UnDash(values["controller"].ToString());
 
             return base.GetHttpHandler(requestContext);
+        }
+
+        /// <summary>
+        /// Converts some/thing-here urls to Some/ThingHere
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private string UnDash(string path)
+        {
+            if (path.Length == 0)
+                return path;
+
+            var sb = new StringBuilder();
+
+            sb.Append(Char.ToUpperInvariant(path[0]));
+
+            for (int i = 1; i < path.Length; i++)
+            {
+                if (path[i] == '-')
+                {
+                    if (i + 1 < path.Length)
+                    {
+                        sb.Append(Char.ToUpperInvariant(path[i + 1]));
+                        i++;
+                    }
+                }
+                else
+                {
+                    sb.Append(path[i]);
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
